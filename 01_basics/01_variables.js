f6bcdeca7d7e5e06b58c06d5a2086e0fcf79a9ec
lokeshgaria <lokeshgaria8811@gmail.com>
2023-12-07 00:04:17 +0530

    learned git 02

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=7823;
+let accountEmail="lokesh@gmail.com";
+var accountPassword="12345"
+
+accountCity="jaipur"
+
+accountEmail="lokeshgaria8811@gmail.com"
+accountPassword="434443"
+accountCity="uk"
+
+console.log(accountId)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let name = "lokesh"
+let age=23;
+let is_loggedIn=true;
+
+
+//number => 2 ,54
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => uniques
+
+//object
+
+console.log(typeof "lokesg")
