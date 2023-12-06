68548e2ba1a7390a9956c2b3c3196e03abaad2be
lokeshgaria <lokeshgaria8811@gmail.com>
2023-12-07 00:18:17 +0530

    chapter 3

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,12 @@
+let score ="33";
+
+let NUMBERScore = Number(score);
+
+console.log(NUMBERScore);
+
+let som = 44;
+
+let stringanumb = String(som)
+
+console.log(stringanumb)
+console.log(typeof stringanumb)
