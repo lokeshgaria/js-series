4a9363c178b2dd7d770a6e8f7c143a98b30e840e
lokeshgaria <lokeshgaria8811@gmail.com>
2023-12-09 11:57:40 +0530

    addde

---
+++ 01_basics/05_String.js
@@ -0,0 +1,9 @@
+const Name = "  lokesh    ";
+
+console.log(Name.charAt(2))
+console.log(Name.substring(0,3))
+console.log(Name.trim())
+
+const URL_ = 'https://lokesh.com/lokesh%20garia'
+
+console.log(URL_.replace('%20','-'))
