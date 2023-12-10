31bddd58d949c418a040c453d0db591694e5060b
lokeshgaria <lokeshgaria8811@gmail.com>
2023-12-10 18:58:42 +0530

    maths and num

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -7,3 +7,16 @@ console.log(Amount.toLocaleString())
 console.log(Amount.toLocaleString('en-IN'))
 
 console.log(balance)
+
+
+//+++++++++++++MATHS+++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4))  //convertd -ve to positive
+
+console.log(Math.ceil(4.3)) // tkes higher value
+
+console.log(Math.floor(4.3)) // tkes lower value
+
+console.log(Math.round(4.3)) //  works as basic round func
