3365cc3a1e8b603cd87199c2da47c2ceb59bc23f
lokeshgaria <lokeshgaria8811@gmail.com>
2023-12-10 18:58:24 +0530

    maths and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,9 @@
+const score=440;
+
+const balance = new Number(100)
+
+const Amount = 2000000;
+console.log(Amount.toLocaleString())
+console.log(Amount.toLocaleString('en-IN'))
+
+console.log(balance)
